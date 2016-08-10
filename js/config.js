f7e4baaa0ef26020dc28b3e9f18dec445cdbba3a
Jason Lestina <coda19@gmail.com>
2016-08-10 15:31:53 -0400

    setting up fetch config

---
+++ js/config.js
@@ -0,0 +1,50 @@
+// API Host
+var api = 'https://gentle-basin-30331.herokuapp.com'
+// var api = 'http://1519054e.ngrok.io'
+
+// Utilities
+// endpoint ... /users
+// formFields ... {name: 'Joe'}
+function fetchApi(method, endpoint, formFields, callback) {
+  var statusCode,
+      payload
+
+  if (method === undefined) {
+    method = 'POST'
+  }
+
+  if (formFields === undefined || formFields === null || formFields === '') {
+    formFields = {}
+  }
+
+  payload = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  if (method.toUpperCase() === 'POST') {
+    formFields[tokenId] = token
+	  payload.body = JSON.stringify(formFields)
+  }
+  else {
+    endpoint += '?' + tokenId + '=' + encodeURIComponent(token)
+  }
+
+  fetch(api + endpoint, payload)
+    .then(function(response) {
+	     statusCode = response.status
+      return response.json()
+    })
+    .then(function(data) {
+      if (typeof callback === 'function') {
+        callback(data, statusCode)
+      }
+    })
+}
+
+
+function redirect(url) {
+  window.location.href = url
+}
