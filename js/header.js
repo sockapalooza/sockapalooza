d66b24a387c17ae970b481ca70e817f736482b92
Tony <gibbsc@indiana.edu>
2016-08-11 14:07:45 -0400

    sign-up page

--- js/header.js
+++ js/header.js
@@ -7,6 +7,8 @@ $(function(){
         }
     });
 
+
+
     $(window).on('beforeunload', function() {
       $(window).scrollTop(210);
     });
@@ -19,56 +21,68 @@ $(function(){
         $('.header-logo').removeClass('transparent');
       }
     });
+
 });
 
-fetchApi('GET', '/products/', {}, (response) => {
-  response.forEach(function(object){
 
-    var nameText = document.createTextNode(object.name)
-    var priceText = document.createTextNode(object.price)
-    var sizeText = document.createTextNode(object.sizes[1].size)
-    //div col-md-4 no-pads cart-item-holder  border
-    var div = document.createElement('div')
-    div.classList.add('col-md-4','no-pads','cart-item-holder','border')
-    //product image img-responsive cart-item
-    var img = document.createElement('img')
-    img.setAttribute('src', object.image)
-    img.classList.add('img-responsive','cart-item')
+$(document).on("click", "#signup", function() {
+    redirect('signup.html');
+});
+
+// fetchApi('GET', '/orders/', {}, (response) => {
+//   response.forEach(function(object){
+//
+//   })
 
 
-    //overlay div
-    var div1 = document.createElement('div')
-    div1.classList.add('col-md-12','cart-item-desc')
-    //prod name
-    var name = document.createElement('h6')
-    name.appendChild(nameText)
-    name.classList.add('add-space')
-    //product price
-    var price = document.createElement('h6')
-    price.appendChild(priceText)
-    price.classList.add('add-space')
-    
-    //prod size
-    var size = document.createElement('h6')
-    size.appendChild(sizeText)
-    size.classList.add('add-space')
-
-    //handles the overlay
-    div1.appendChild(name)
-    div1.appendChild(price)
-    div1.appendChild(size)
-    //main div
-    div.appendChild(img)
-    div.appendChild(div1)
-
-    document.querySelector('.modal-cart-content').appendChild(div)
-
-
+fetchApi('GET', '/orders', {}, (response) => {
+console.log(response);
   })
 
-
-  //prod size
-  //prod price
-  //prod name
-
-})
+// fetchApi('GET', '/orders/', {}, (response) => {
+//   response.forEach(function(object){
+//
+//     var nameText = document.createTextNode(object.name)
+//     var priceText = document.createTextNode(object.price)
+//     var sizeText = document.createTextNode(object.sizes[1].size)
+//     //div col-md-4 no-pads cart-item-holder  border
+//     var div = document.createElement('div')
+//     div.classList.add('col-md-4','no-pads','cart-item-holder','border')
+//     //product image img-responsive cart-item
+//     var img = document.createElement('img')
+//     img.setAttribute('src', object.image)
+//     img.classList.add('img-responsive','cart-item')
+//
+//
+//     //overlay div
+//     var div1 = document.createElement('div')
+//     div1.classList.add('col-md-12','cart-item-desc')
+//     //prod name
+//     var name = document.createElement('h6')
+//     name.appendChild(nameText)
+//     name.classList.add('add-space')
+//     //product price
+//     var price = document.createElement('h6')
+//     price.appendChild(priceText)
+//     price.classList.add('add-space')
+//
+//     //prod size
+//     var size = document.createElement('h6')
+//     size.appendChild(sizeText)
+//     size.classList.add('add-space')
+//
+//     //handles the overlay
+//     div1.appendChild(name)
+//     div1.appendChild(price)
+//     div1.appendChild(size)
+//     //main div
+//     div.appendChild(img)
+//     div.appendChild(div1)
+//
+//     console.log(object);
+//
+//     document.querySelector('.modal-cart-content').appendChild(div)
+//
+//
+//   })
+// })
